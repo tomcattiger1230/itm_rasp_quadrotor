7d27ec515cba83e0236b8923aa871e46fcc6e7f7
tomcattigerkkk <wei.luo.88@outlook.com>
2022-04-01 13:19:01 +0200

    add ax12 controller, but not test yet

--- script/ax12_control.py
+++ script/ax12_control.py
@@ -4,13 +4,14 @@
 Author: Wei Luo
 Date: 2022-04-01 00:38:00
 LastEditors: Wei Luo
-LastEditTime: 2022-04-01 00:42:11
+LastEditTime: 2022-04-01 13:18:09
 Note: Note
 '''
 
 import rospy
 import numpy as np
 from pyax12.connection import Connection
+from itm_mav_msgs.msg import itm_trajectory_msg
 
 
 class AX12Controller(object):
@@ -19,6 +20,22 @@ class AX12Controller(object):
         self.serial_connection = Connection(port=serial_port, baudrate=57600)
         # incase multiple dynamixel motors
         self.dynamixel_id = dynamixel_id
+        # subscribe trajectory
+        self.sub_trajectory = rospy.Subscriber('/robot_trajectory',
+                                               itm_trajectory_msg,
+                                               self.traj_callback,
+                                               queue_size=10)
+        self.reference_alpha = np.pi / 2.0
+        self.reference_alpha_rate = 0.0
+
+    def traj_callback(self, msg):
+        self.reference_alpha = msg.traj[0].alpha
+        self.reference_alpha_rate = msg.traj[0].alpha_rate
+
+    def progress(self, ):
+        self.serial_connection.goto(self.dynamixel_id,
+                                    self.reference_alpha,
+                                    speed=300)
 
 
 if __name__ == '__main__':
@@ -30,7 +47,11 @@ if __name__ == '__main__':
 
     ax12_obj = AX12Controller(serial_port)
 
-    while not rospy.is_shutdown():
-        pass
+    rate = rospy.Rate(10)
 
+    while not rospy.is_shutdown():
+        ax12_obj.progress()
+        rate.sleep()
+
+    ax12_obj.serial_connection.close()
     rospy.loginfo('AX12 controller is closed')
